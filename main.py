6211b41ca9f8d977bcb7f08b484b958f92d8386c
esimoes <emilianosimoes@gmail.com>
2024-11-09 07:46:53 -0300

    update python-telegram-bot

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
     job_queue = application.job_queue
     # Create a job that runs daily at 9 a.m.
     timezone = datetime.timezone(datetime.timedelta(hours=-3))
-    job_queue.run_daily(daily_update, time=datetime.time(hour=8, minute=0,tzinfo=timezone))
+    job_queue.run_daily(daily_update, time=datetime.time(hour=9, minute=0,tzinfo=timezone))
 
     # Register commands
     application.add_handler(start_command_handler)
